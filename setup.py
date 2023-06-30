49876d090eb73c487d60c949af8b7212e253ff65
TrazTech <127814143+traztech@users.noreply.github.com>
2023-06-29 23:54:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trazhash',
-    version='0.1.0',
+    version='0.1.3',
     packages=find_packages(),
     install_requires=[
         # List your library dependencies here
